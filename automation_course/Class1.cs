3629e78b9a2e73a8be00f3d32049e6231a642754
Mahalon Ochieng <Mahalon.Ochieng@ding.com>
2024-11-01 09:54:10 +0300

    added login code

--- automation_course/Class1.cs
+++ automation_course/Class1.cs
@@ -18,5 +18,46 @@ namespace automation_course
             }
         }
 
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void shouldBeAbleToLogInSuccessfully()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                String url = "https://www.saucedemo.com/";
+                webDriver.Navigate().GoToUrl(url);
+                IWebElement usernameInput = webDriver.FindElement(By.Id("user-name"));
+                IWebElement passwordInput = webDriver.FindElement(By.Id("password"));
+                IWebElement loginInput = webDriver.FindElement(By.Name("login-button"));
+
+                usernameInput.SendKeys("standard_user");
+                passwordInput.SendKeys("secret_sauce");
+                loginInput.Click();
+                Assert.Equal("https://www.saucedemo.com/inventory.html", webDriver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void shouldNotBeAbleToLogInWithoutPassword()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                String url = "https://www.saucedemo.com/";
+                webDriver.Navigate().GoToUrl(url);
+                IWebElement usernameInput = webDriver.FindElement(By.Id("user-name"));
+                IWebElement loginInput = webDriver.FindElement(By.Name("login-button"));
+
+                usernameInput.SendKeys("standard_user");
+                IWebElement errorMessage = webDriver.FindElement(By.CssSelector("h3[data-test='error']"));
+                loginInput.Click();
+                bool isErrorMessageVisible = errorMessage.Displayed;
+                String errorMessageText=errorMessage.Text;
+                Assert.True(isErrorMessageVisible);
+                Assert.Equal("https://www.saucedemo.com/", webDriver.Url);
+                Assert.Equal(errorMessageText, "Epic sadface: Password is required");
+            }
+        }
+
     }
 }
