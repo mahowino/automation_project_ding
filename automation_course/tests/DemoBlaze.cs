b250e76b5ee0a7052e688b9f3f60fbe357a8e353
Mahalon Ochieng <Mahalon.Ochieng@ding.com>
2024-11-12 18:23:49 +0300

    added page object model and started structure for demo blaze test.

---
+++ automation_course/tests/DemoBlaze.cs
@@ -0,0 +1,32 @@
+﻿
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using automation_course.pages.demoBlaze;
+using automation_course.pages.demoBlaze.models;
+namespace automation_course.tests
+{
+    [TestFixture]
+
+    public class DemoBlaze
+    {
+        DemoBlazeHomePage demoBlazeHomePage = new();
+
+    
+
+        [Test]
+        public void shouldBeAbleToSendAContactUsMessage()
+        {
+            demoBlazeHomePage.setUpBeforeEachTest();
+            demoBlazeHomePage.loadHomePage();
+            demoBlazeHomePage.clickContactUsFromNavigationBar();
+            ContactUsForm contactUsForm = new ContactUsForm("mahowino@gmail.com", "mahowino", "test");
+            demoBlazeHomePage.submitContactUsModal(contactUsForm);
+            demoBlazeHomePage.cleanUpAfterTest();
+            demoBlazeHomePage.cleanUpAfterTest();
+        }
+
+    }
+}
